5ae278a365f6275bba05d596fbc87e44afa8d96a
oliverlowbob <oliverlowbob@live.dk>
2021-04-26 21:19:18 +0200

    refactoring to userrepository

--- Skoleprotokol/Controllers/UserController.cs
+++ Skoleprotokol/Controllers/UserController.cs
@@ -12,6 +12,8 @@ namespace Skoleprotokol.Controllers
     public class UserController : ApiController
     {
 
+        UserRepository userRepository = new UserRepository();
+
         public UserController()
         {
         }
@@ -20,18 +22,6 @@ namespace Skoleprotokol.Controllers
         [Route("users")]
         public async Task UpdateUser([FromBody] UserApiModel args)
         {
-            var userRepository = new UserRepository();
-
-            if(args == null)
-            {
-                throw new ArgumentNullException("No args given");
-            }
-
-            if(args.Id == 0)
-            {
-                throw new ArgumentNullException("No user id");
-            }
-
             await userRepository.UpdateUser(args);
         }
 
@@ -39,101 +29,21 @@ namespace Skoleprotokol.Controllers
         [Route("users/enable/{userId}")]
         public async Task EnableUser(int userId)
         {
-            using (var dbContext = new Scool_ProtocolContext())
-            {
-                var transaction = await dbContext.Database.BeginTransactionAsync();
-
-                var user = await dbContext.Users.FindAsync(userId);
-
-                if(user.Active == true)
-                {
-                    return;
-                }
-
-                user.Active = true;
-
-                await dbContext.SaveChangesAsync();
-
-                await transaction.CommitAsync();
-            }
+            await userRepository.EnableUser(userId);
         }
-
 
         [HttpPost]
         [Route("users/disable/{userId}")]
         public async Task DisableUser(int userId)
         {
-            using (var dbContext = new Scool_ProtocolContext())
-            {
-                var transaction = await dbContext.Database.BeginTransactionAsync();
-
-                var user = await dbContext.Users.FindAsync(userId);
-
-                if(user.Active == false)
-                {
-                    return;
-                }
-
-                user.Active = false;
-
-                await dbContext.SaveChangesAsync();
-
-                await transaction.CommitAsync();
-            }
+            await userRepository.DisableUser(userId);
         }
 
         [HttpGet]
         [Route("users/{userId}")]
         public async Task<UserApiModel> GetUser(int userId)
         {
-            using (var dbContext = new Scool_ProtocolContext())
-            {
-                var transaction = await dbContext.Database.BeginTransactionAsync();
-
-                var user = await dbContext.Users.FindAsync(userId);
-
-                var result = new UserApiModel
-                {
-                    Id = user.Iduser,
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    Email = user.Email,
-                    Password = user.Password,
-                    Active = user.Active,
-                    SchoolId = user.SchoolIdschool,
-                    School = new SchoolApiModel
-                    {
-                        Id = user.SchoolIdschoolNavigation.Idschool,
-                        Name = user.SchoolIdschoolNavigation.Name
-                    },
-                    Lessons = user.Lessons.Select(l => new LessonApiModel
-                    {
-                        Class = new ClassApiModel
-                        {
-                            End = l.ClassIdclassNavigation.End,
-                            Start = l.ClassIdclassNavigation.Start,
-                            Id = l.ClassIdclass,
-                            NumberOfClass = l.ClassIdclassNavigation.NumberOfClass,
-                            Course = new CourseApiModel
-                            {
-                                Id = l.ClassIdclassNavigation.CourseIdcourse,
-                                Name = l.ClassIdclassNavigation.CourseIdcourseNavigation.Name
-                            }
-                        },
-                        Present = l.Present
-
-                    }).ToList(),
-                    Roles = user.UserRoles.Select(ur => new RoleApiModel
-                    {
-                        Id = ur.RoleIdrole,
-                        Name = ur.RoleIdroleNavigation.Role1
-                    }).ToList()
-                };
-
-                await transaction.CommitAsync();
-
-                return result;
-            }
+            return await userRepository.GetUser(userId);
         }
     }
 }
