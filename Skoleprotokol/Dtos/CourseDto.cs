955427188e3c9704872ccddb21a80abc34004219
andreasdan <andreas.petersen01@gmail.com>
2021-05-20 22:44:01 +0200

    added classes collection to coursedto

--- Skoleprotokol/Dtos/CourseDto.cs
+++ Skoleprotokol/Dtos/CourseDto.cs
@@ -9,5 +9,6 @@ namespace Skoleprotokol.Dtos
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public IEnumerable<ClassDto> Classes { get; set; }
     }
 }
