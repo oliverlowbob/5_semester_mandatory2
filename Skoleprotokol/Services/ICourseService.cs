b90cd5ae1b6b20f383f844c479dfa7d10dd59928
Jonasjoja <43268787+Jonasjoja@users.noreply.github.com>
2021-05-20 18:19:19 +0200

    Get course by ID

---
+++ Skoleprotokol/Services/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skoleprotokol.Services
+{
+    public interface ICourseService<TCourse>
+    {
+        Task<TCourse> GetCourseById(int courseId);
+    }
+}
